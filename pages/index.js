95c68532b004d472f445314e85ce79cd66ec5844
Nattapon Rattajak <rattajak.n@gmail.com>
2017-12-11 13:48:09 +0700

    add ads header.

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,23 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const React = require("react");
 const flexbox_react_1 = require("flexbox-react");
+const head_1 = require("next/head");
 const withData_1 = require("../lib/withData");
 const withMaterialUI_1 = require("../lib/withMaterialUI");
 const responsiveHelper_1 = require("../src/utils/responsiveHelper");
 const Programs_1 = require("../src/containers/Programs");
-const Index = (props) => (React.createElement(flexbox_react_1.default, { flexDirection: "row", justifyContent: "center", height: "100%" },
-    React.createElement(flexbox_react_1.default, null),
-    React.createElement("div", { id: "home" },
-        console.log("LOG", props, responsiveHelper_1.getScreen()),
-        React.createElement(Programs_1.default, null)),
-    React.createElement(flexbox_react_1.default, null)));
+const Index = (props) => (React.createElement("div", null,
+    React.createElement(head_1.default, null,
+        React.createElement("title", null, "dootoons.com"),
+        React.createElement("meta", { name: "viewport", content: "initial-scale=1.0, width=device-width" }),
+        React.createElement("script", { async: true, src: "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" }),
+        React.createElement("script", { dangerouslySetInnerHTML: { __html: `(adsbygoogle = window.adsbygoogle || []).push({google_ad_client: "ca-pub-6158055273584096",enable_page_level_ads: true});` } })),
+    React.createElement(flexbox_react_1.default, { flexDirection: "row", justifyContent: "center", height: "100%" },
+        React.createElement(flexbox_react_1.default, null),
+        React.createElement("div", { id: "home" },
+            console.log("LOG", props, responsiveHelper_1.getScreen()),
+            React.createElement(Programs_1.default, null)),
+        React.createElement(flexbox_react_1.default, null))));
 const HomePage = withMaterialUI_1.default(Index);
 const HomeWithData = withData_1.default(HomePage);
 exports.default = HomeWithData;
--- pages/seasons.tsx
+++ pages/seasons.tsx
@@ -5,6 +5,7 @@ import { List, ListItem } from "material-ui/List";
 
 import Router from "next/router";
 import Flexbox from "flexbox-react";
+import Head from 'next/head';
 
 import { getScreen } from "../src/utils/responsiveHelper";
 import { IContentProps, IRouteProps } from "../src/structs/Structs";
@@ -26,13 +27,21 @@ class Seasons extends React.Component<ISeasonPageProps, any> {
 
     render() {
         return (
-            <Flexbox flexDirection="row" justifyContent="center" height={"100%"}>
-                <Flexbox />
-                <div id="seasons">
-                    <ContentList {...this.props} onClickContent={this.onClickItem} />
-                </div>
-                <Flexbox />
-            </Flexbox >
+            <div>
+                <Head>
+                    <title>dootoons.com</title>
+                    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                    <script dangerouslySetInnerHTML={{ __html: `(adsbygoogle = window.adsbygoogle || []).push({google_ad_client: "ca-pub-6158055273584096",enable_page_level_ads: true});` }} />
+                </Head>
+                <Flexbox flexDirection="row" justifyContent="center" height={"100%"}>
+                    <Flexbox />
+                    <div id="seasons">
+                        <ContentList {...this.props} onClickContent={this.onClickItem} />
+                    </div>
+                    <Flexbox />
+                </Flexbox >
+            </div>
         );
     }
 }
